a68923be36175e3cb5f3f8a4507a44c361ec2772
aviNelson <valera.zz73@gmail.com>
2023-01-18 17:14:25 +0300

    Init project

---
+++ src/main/java/com/avinelson/microservicesbookstore/MicroservicesBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.avinelson.microservicesbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesBookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avinelson/microservicesbookstore/MicroservicesBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinelson.microservicesbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
